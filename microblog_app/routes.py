47eff0ae7e69a6ef015bcc50b35cc830f6df6b72
Maksim Ivanov <ivanovmaximant@gmail.com>
2020-11-08 15:38:28 +0200

    Create base template

--- microblog_app/routes.py
+++ microblog_app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from microblog_app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "microblog"
+    user = {'username': 'Max'}
+    posts = [
+        {
+            'author': {'username': 'User1'},
+            'body': 'Test message 1!'
+        },
+        {
+            'author': {'username': 'User2'},
+            'body': 'Test message 2!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
