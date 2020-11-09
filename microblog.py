f59c7eb2a6390e8e797657f3a0732ac10bdbbff2
Maksim Ivanov <ivanovmaximant@gmail.com>
2020-11-09 18:15:05 +0200

    Connect database engine

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from microblog_app import app
+from microblog_app import app, db
+from microblog_app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
