09b5d76f8dd36761d3fdef9aaa3b62a85f1920ec
Maksim Ivanov <ivanovmaximant@gmail.com>
2020-11-14 23:28:35 +0200

    Add posting feature and pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get('ADMINS')
+    POSTS_PER_PAGE = 25
